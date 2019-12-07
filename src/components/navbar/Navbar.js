1105229f7079d39989b9fb8a1d09077dc364b79e
Uros Cirkovic <cirkovic.uros.srb@gmail.com>
2019-12-08 00:46:23 +0100

    Update function component name to uppercase

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,18 +3,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const navbar = () => {
+const Navbar = () => {
   return (
     <nav className="nav-style">
       <ul>
         <li>
-          <Link to="/projects">
-            Projects
-          </Link>
+          <Link to="/projects">Projects</Link>
         </li>
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default navbar;
+export default Navbar;
